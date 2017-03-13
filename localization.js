b6b93909c8e2a5576e3eb16b23c0c13e54037cc6
Agrigoroae Maxim <jasonrust89@gmail.com>
2017-03-13 15:58:10 +0200

    Access only "GET" methods only

--- localization.js
+++ localization.js
@@ -2,6 +2,12 @@ let Translator = require('./index');
 const DEFAULT_LOCALE = 'en';
 
 module.exports = function * (req, res, next) {
+    if(req.method != 'GET') 
+    {
+        // skip post, put, delete methods.
+        return next();
+    }
+    
     let locale = req.params.lang;
 
     if(locale)
@@ -11,6 +17,7 @@ module.exports = function * (req, res, next) {
         req.translator = (new Translator(req.session.lang));
 
     } else {
+
         req.session.lang = req.session.lang || DEFAULT_LOCALE;    
 
         return res.redirect(`/${req.session.lang}${req.originalUrl}`);
